6e15610bfa6341bfde92cdb09177db9bad7a0c01
WheatBox <wheat_box@outlook.com>
2024-11-10 17:55:53 +0800

    cmake-windows.bat 改进

--- cmake-windows.bat
+++ cmake-windows.bat
@@ -1,2 +1,17 @@
-cmake -S . -B Build -DCMAKE_TOOLCHAIN_FILE=C:/DevPacks/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
+@echo off
+setlocal
+
+:: Change this value so that the file name it stores points to your vcpkg.cmake
+set "vcpkg_cmake=C:/DevPacks/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+if exist "%vcpkg_cmake%" (
+	cmake -S . -B Build -DCMAKE_TOOLCHAIN_FILE="%vcpkg_cmake%" -DVCPKG_TARGET_TRIPLET=x64-windows-static
+) else (
+	echo +-------------------------------------------------------------
+	echo ^| "%vcpkg_cmake%" not exists.
+	echo ^| Edit cmake-windows.bat and modify the value of vcpkg_cmake.
+	echo +-------------------------------------------------------------
+)
+
+endlocal
 pause
