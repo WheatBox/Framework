813d8a058cbad049e2aac40a65a0085c9ee9ce57
WheatBox <wheat_box@outlook.com>
2024-11-10 19:16:57 +0800

    cmake-linux.sh 改进

--- cmake-linux.sh
+++ cmake-linux.sh
@@ -1,1 +1,15 @@
-cmake -S . -B Build -DPlatform=Linux -DCMAKE_TOOLCHAIN_FILE=/home/wheat_box/DevPacks/vcpkg/scripts/buildsystems/vcpkg.cmake
+#!/bin/bash
+
+# Change this value so that the file name it stores points to your vcpkg.cmake
+vcpkg_cmake="/home/wheat_box/DevPacks/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+if [ -e "$vcpkg_cmake" ]; then
+	cmake -S . -B Build -DCMAKE_TOOLCHAIN_FILE="$vcpkg_cmake"
+else
+	echo +-------------------------------------------------------------
+	echo "|" "$vcpkg_cmake" not exists.
+	echo "|" Edit cmake-windows.bat and modify the value of vcpkg_cmake.
+	echo +-------------------------------------------------------------
+fi
+
+read -p "Press [Enter] key to continue..."
