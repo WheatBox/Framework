49b652641291ceba3893d0510b98d763fe4afa5f
WheatBox <wheat_box@outlook.com>
2024-12-03 13:05:20 +0800

    新增 mingw 支持

---
+++ cmake-mingw.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+:: Change this value so that the file name it stores points to your vcpkg.cmake
+set "vcpkg_cmake=C:/DevPacks/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+if exist "%vcpkg_cmake%" (
+	cmake -S . -B Build_mingw -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="%vcpkg_cmake%" -DVCPKG_TARGET_TRIPLET=x64-mingw-static -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+	move Build_mingw\compile_commands.json .\
+) else (
+	echo +-------------------------------------------------------------
+	echo ^| "%vcpkg_cmake%" not exists.
+	echo ^| Edit cmake-windows.bat and modify the value of vcpkg_cmake.
+	echo +-------------------------------------------------------------
+)
+
+endlocal
+pause
